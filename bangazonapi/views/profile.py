8b2745327b08871427a6343e3febdefdf6659322
Ryan Beiden <rdbeiden@gmail.com>
2021-02-09 20:03:00 -0600

    remove duplicate line_item key

--- bangazonapi/views/profile.py
+++ bangazonapi/views/profile.py
@@ -156,7 +156,6 @@ class Profile(ViewSet):
 
                 cart = {}
                 cart["order"] = OrderSerializer(open_order, many=False, context={'request': request}).data
-                cart["order"]["line_items"] = line_items.data
                 cart["order"]["size"] = len(line_items.data)
 
 
