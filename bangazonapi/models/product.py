80baf2c6be4cdd5814bfe165aac5283b45c2493e
Ryan Beiden <rdbeiden@gmail.com>
2021-02-09 19:44:14 -0600

    add exception name for average rating

--- bangazonapi/models/product.py
+++ bangazonapi/models/product.py
@@ -65,7 +65,7 @@ class Product(SafeDeleteModel):
 
         try:
             avg = total_rating / len(ratings)
-        except:
+        except ZeroDivisionError:
             avg = 0
             
         return avg
